482e6f1ab0d960a81a03b254e3598caa3897b3e5
Thom-fs <thomas.schneider29@outlook.fr>
2022-07-20 11:17:03 +0200

    p

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,47 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ConnexionView from "../views/ConnexionView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: {
+      title: "Accueil",
+    },
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    meta: {
+      title: "A propos",
+    },
+  },
+  {
+    path: "/connexion",
+    name: "Connexion",
+    component: ConnexionView,
+    meta: {
+      title: "Connexion",
+    },
+  },
+  {
+    path: "/:pathMatch(.*)",
+    name: "NotFound",
+    component: NotFoundView,
+    meta: {
+      title: "404 Not Found",
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
