0ec0a5c6c386a07dcf6beb2ec9d33ed70cd83730
otherKevin <kevin-cariou@hotmail.fr>
2022-07-22 12:09:06 +0200

    Commit avec moins de danger

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import EventUniqueView from "../views/EventUniqueView.vue";
 import ProfilView from "../views/ProfilView.vue";
 import CreateSlotsView from "../views/CreateSlotsView.vue";
 import CreateUserView from "../views/CreateUserView.vue";
+import GroupUniqueView from "../views/GroupUniqueView.vue";
 
 const routes = [
   {
